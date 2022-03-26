f2867fedfab2d6aa56a63f3edf705b6e8bdee6c9
RunnanC-137 <ruanlons@gmail.com>
2022-03-26 13:33:08 -0300

    up

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,59 @@
+
 const sorteio = document.querySelector(".sorteio");
 const numeroSorteado = document.querySelector(".numero-sorteado");
-const opcoesDeAnimais = document.querySelector(".opcoes")
+const table = document.querySelector(".table")
 
-const animais = ["vaca", "jacare", "onça", "pavão", "cavalo"]
+const animailEscolhido = document.querySelector(".animail-escolhido")
+const numeroEscolhido = document.querySelector(".numero-escolhido")
 
-for (let index in animais) {
-    
-    opcoesDeAnimais.innerHTML += `<option value="${animais[index]}">${animais[index]}</option>`
+
+const animalsList = {
+    Avestrus:["01",],
+    Aguia:["02",],
+    Burro:["03",],
+    Borboleta:["04",],
+    Cachorro:["05",],
+
+    Cabra:["06",],
+    Carneiro:["07",],
+    Camelo:["08",],
+    Cobra:["09",],
+    Coelho:["10"],
+
+    Cavalo:["11"],
+    Elefante:["12"],
+    Galo:["13"],
+    Gato:["14"],
+    Jacare:["15"],
+
+    Leao:["16"],
+    Mamaco:["17"],
+    Porco:["18"],
+    Pavao:["19"],
+    Peru:["20"],
+
+    Touro:["21"],
+    Tigre:["22"],
+    Urso:["23"],
+    Veado:["24"],
+    Vaca:["25"]
 
 }
 
-sorteio.addEventListener( "click", element => {
 
-    let animal = animais[Math.floor((Math.random()) * 4)]
-    let aposta = document.querySelector(".opcoes").value
-    numeroSorteado.innerHTML = animal;
 
-    animal == aposta ? alert("Você ganhou") : alert("Você perdeu")
+
+/// escopo dos laços
+Object.keys(animalsList).forEach(element => {
+    
+    table.innerHTML += `<div class="animal">
+    <h2 class="numero">${animalsList[element][0]}</h2>
+    <h1 class="nome">${element}</h1>
+    </div>`
 });
+
+const animais = document.querySelectorAll(".animal")
+console.log(animais)
+
+
+

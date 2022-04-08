0d6bcde47323172a746a5eff33db4eada9ed2e32
RunnanC-137 <ruanlons@gmail.com>
2022-04-08 14:50:33 -0300

    up

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,12 @@
-const numeroSorteado = document.querySelector(".numero-sorteado");
 const table = document.querySelector(".table")
 
 const animailEscolhido = document.querySelector(".animal-escolhido")
 const numeroEscolhido = document.querySelector(".numero-escolhido")
 
+const escolherAnimal = document.querySelector(".escolher-animal")
+const escolherNumero = document.querySelector(".escolher-numero")
+
+const iniciar = document.querySelector(".iniciar")
 
 const animalsList = {
     Avestrus:["01",],
@@ -68,35 +71,74 @@ animais.forEach(i => {
 
             numeroEscolhido.innerHTML = `${i.children[0].textContent}`
         })
+})
 
+iniciar.addEventListener("click", () => {
+
+    let animal = Math.floor(Math.random() * 25)
+    let numero = Math.floor(Math.random() * 25)
+    animal = Object.keys(animalsList)[animal]
+    numero = Object.values(animalsList)[numero][0]
+
+    escolherAnimal.innerHTML = animal
+    escolherNumero.innerHTML = numero
+
+    ani = animailEscolhido.textContent
+    num = numeroEscolhido.textContent 
+
+    if (ani == animal) {
+        alert("vocé acertou o animal")
+    } else if (num == numero) {
+
+        alert("vocé acertou o número")
+
+    }
+    
 })
 
 
     
+const webSites = {
+    
+    
+    criadorDeCurriculo: {
+        site:"https://runnanc-137.github.io/WebSite-s/Criador_De_Curriculo",
+        nome:"Criador de currículo",
+        concluido: false,
+        descricao: "Aplicação criada com o intuito de ajudar quem não tem um currículo cria um de forma fácil. Projeto criado para treinar as habilidades de DOM e experiência do usuário.",
+        img: "imgs/sites.png.opdownload"
+    },
+
+    
+    
+    simpleProjects:{
+        site:"https://runnanc-137.github.io/WebSite-s/projetinhos",
+        nome:"Simples projects",
+        concluido: false,
+        descricao: "Aqui eu crio projetinhos de pequeno porte para fins específicos, são projetos simples execultados com HTML, CSS e JavaScript.",
+        img: "imgs/pngegg.png"
+    }
+}
 
 
+function indexBusc (object, chave=""){
+    
+    chave = object[chave]
+    let contador = 0
+    for (index in object) {
 
-/* animais.forEach(i => {
+        if (object[index] == chave) {
+            return contador
+            break 
+        } else {
 
-    i.addEventListener( "click" , o => {
-
-        console.log(o)
-        animailEscolhido.innerHTML = `${i.children[1].textContent}`
-        numeroEscolhido.innerHTML = `${i.children[0].textContent}`
-    })
-}) */
-
-
-
-
-
-
-
-
-
-
+            contador++
+        }
+ 
+    }
 
 }
 
 
 
+}
